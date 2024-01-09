fc5ca4cb09465a59e70f407360c371170e0e88ba
Danilo R <danilorodrigo@gmail.com>
2024-01-08 22:42:28 -0400

    First lesson

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Virtual Machine Java
+-   LTS - Long Term Support 
 -  OpenJDK-11.0.21
---
+++ workspace/curso_programacao/src/curso_programacao/Main.java
@@ -0,0 +1,11 @@
+package curso_programacao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testando um programa JAVA");
+	}
+
+}
